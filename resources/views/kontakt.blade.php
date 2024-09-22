34da8ca6f2edccc88f9b72cd0403f2ff2ba510b5
Tomasz Nowacki <tomasz.nowacki@wsb.poznan.pl>
2024-09-22 13:16:11 +0200

    Widoki 22.09.2024

---
+++ resources/views/kontakt.blade.php
@@ -0,0 +1,13 @@
+@extends('layout')
+@section('tytul')
+    Kontakt
+@endsection
+@section('podtytul')
+    Kontakt w podtytule
+@endsection
+@section('tresc')
+    <div class="title m-b-md">
+        Strona kontaktu
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae, cumque dolorem saepe vitae nihil, nam mollitia assumenda molestias aliquid eaque fugiat expedita ducimus! Eos cumque aliquid tenetur recusandae repudiandae illo?
+    </div>
+@endsection
---
+++ resources/views/onas.blade.php
@@ -0,0 +1,13 @@
+@extends('layout')
+@section('tytul')
+    O nas
+@endsection
+@section('podtytul')
+    O nas w podtytule
+@endsection
+@section('tresc')
+    <div class="title m-b-md">
+        Strona o nas 
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione numquam illo ipsam aperiam. Perspiciatis ut cum eius totam, repellat necessitatibus provident porro commodi aliquam consectetur. Nemo commodi quas cum labore.
+    </div>
+@endsection
